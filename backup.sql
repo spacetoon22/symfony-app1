332f830fb495282f97baea7d95a03e3069c199eb
spacetoon22 <aminefoot100@gmail.com>
2026-03-24 16:56:07 +0100

    update project

--- backup.sql
+++ backup.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.45, for Linux (x86_64)
 --
--- Host: localhost    Database: symfony_db
+-- Host: localhost    Database: symfony_app
 -- ------------------------------------------------------
 -- Server version	8.0.45-0ubuntu0.24.04.1
 
@@ -36,7 +36,7 @@ CREATE TABLE `doctrine_migration_versions` (
 
 LOCK TABLES `doctrine_migration_versions` WRITE;
 /*!40000 ALTER TABLE `doctrine_migration_versions` DISABLE KEYS */;
-INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20260217170431','2026-02-24 14:43:02',25),('DoctrineMigrations\\Version20260218113017','2026-02-24 14:43:02',49),('DoctrineMigrations\\Version20260218122033',NULL,NULL),('DoctrineMigrations\\Version20260218141934',NULL,NULL),('DoctrineMigrations\\Version20260223145722',NULL,NULL),('DoctrineMigrations\\Version20260224090825',NULL,NULL),('DoctrineMigrations\\Version20260224102917',NULL,NULL),('DoctrineMigrations\\Version20260224144038',NULL,NULL),('DoctrineMigrations\\Version20260224144256',NULL,NULL),('DoctrineMigrations\\Version20260224144650','2026-02-24 14:46:51',502),('DoctrineMigrations\\Version20260224153732','2026-02-24 15:37:33',21),('DoctrineMigrations\\Version20260226133831','2026-02-26 13:38:32',163),('DoctrineMigrations\\Version20260227134708','2026-02-27 13:47:14',63),('DoctrineMigrations\\Version20260303153821','2026-03-03 15:38:22',28),('DoctrineMigrations\\Version20260304150447','2026-03-04 15:04:54',23);
+INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20260217170431','2026-02-24 14:43:02',25),('DoctrineMigrations\\Version20260218113017','2026-02-24 14:43:02',49),('DoctrineMigrations\\Version20260218122033',NULL,NULL),('DoctrineMigrations\\Version20260218141934',NULL,NULL),('DoctrineMigrations\\Version20260223145722',NULL,NULL),('DoctrineMigrations\\Version20260224090825',NULL,NULL),('DoctrineMigrations\\Version20260224102917',NULL,NULL),('DoctrineMigrations\\Version20260224144038',NULL,NULL),('DoctrineMigrations\\Version20260224144256',NULL,NULL),('DoctrineMigrations\\Version20260224144650','2026-02-24 14:46:51',502),('DoctrineMigrations\\Version20260224153732','2026-02-24 15:37:33',21),('DoctrineMigrations\\Version20260226133831','2026-02-26 13:38:32',163),('DoctrineMigrations\\Version20260227134708','2026-02-27 13:47:14',63),('DoctrineMigrations\\Version20260303153821','2026-03-03 15:38:22',28),('DoctrineMigrations\\Version20260304150447','2026-03-04 15:04:54',23),('DoctrineMigrations\\Version20260310142501','2026-03-24 13:40:57',38),('DoctrineMigrations\\Version20260310152622','2026-03-24 13:40:57',61);
 /*!40000 ALTER TABLE `doctrine_migration_versions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -49,9 +49,9 @@ DROP TABLE IF EXISTS `document`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `document` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `file_name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `file_path` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `file_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `file_path` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `uploaded_at` datetime NOT NULL,
   `prospect_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -78,36 +78,39 @@ DROP TABLE IF EXISTS `dossier`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `dossier` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `last_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `cin` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `gender` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `first_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cin` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `gender` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `dob` date DEFAULT NULL,
-  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `city` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `address` longtext COLLATE utf8mb4_unicode_ci,
-  `plan` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(180) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `plan` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `start_date` date DEFAULT NULL,
   `beneficiaries` int DEFAULT NULL,
-  `conditions` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `conditions` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `premium` double DEFAULT NULL,
-  `medical_notes` longtext COLLATE utf8mb4_unicode_ci,
-  `commercial_notes` longtext COLLATE utf8mb4_unicode_ci,
+  `medical_notes` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `commercial_notes` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `documents` json DEFAULT NULL,
-  `status` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `rejection_note` longtext COLLATE utf8mb4_unicode_ci,
+  `status` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rejection_note` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `created_at` datetime NOT NULL,
   `claimed_by_id` int DEFAULT NULL,
   `uploaded_files` json DEFAULT NULL,
   `assigned_to_id` int DEFAULT NULL,
-  `type` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `payment_method` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `duration_months` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_3D48E037F67E7A38` (`claimed_by_id`),
   KEY `IDX_3D48E037F4BD7827` (`assigned_to_id`),
   CONSTRAINT `FK_3D48E037F4BD7827` FOREIGN KEY (`assigned_to_id`) REFERENCES `user` (`id`),
   CONSTRAINT `FK_3D48E037F67E7A38` FOREIGN KEY (`claimed_by_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,7 +119,7 @@ CREATE TABLE `dossier` (
 
 LOCK TABLES `dossier` WRITE;
 /*!40000 ALTER TABLE `dossier` DISABLE KEYS */;
-INSERT INTO `dossier` VALUES (11,'way2','way2','ae456598','male','2026-02-07','alex@gmail.ma','0600000000','way2','','basic','2026-01-31',1,'diabetes',6,'','','[\"cin\", \"contract\"]','approved',NULL,'2026-02-26 13:45:32',11,'[\"1772113532_726f7cb8_NumPy_Python_DS_2026.pdf\", \"1772113532_cab46071_DEVIS_AKSAM_CASQUE_IPPHONE_.pdf\"]',11,NULL),(12,'karim','li','ae456598','female','6999-05-15','karim@gmai.ma','0600000000','casa','','premium','1899-08-19',3,'hypertension',320,'','','[\"photo\", \"contract\"]','approved',NULL,'2026-02-26 15:51:22',9,'[\"1772121082_215cf502_PW1_-_Class_diagrams.pdf\", \"69a06c2c7cd13_signature.PNG\", \"69a06c2c7ce27_The-Standard-CBIC-Medicaid-Card.png\", \"69a06c3cecbe5_FICHIER DE PASSATIO.pdf\", \"69a06cdf74975_key programme.PNG\"]',9,NULL),(13,'Nada','kira','ab1123','female','1994-03-02','Nada@gmail.ma','0600000055','fes','','family','2026-04-01',1,'other',1500,'Cancer','','[]','approved',NULL,'2026-03-02 11:01:58',11,'[\"69a56ec24f618_The-Standard-CBIC-Medicaid-Card.png\"]',11,NULL),(14,'Mane','kalulu','D6925','male','1980-03-19','mane@gmail.ma','0600000055','casa','','basic','2026-03-19',1,'none',450,'','','[]','rejected','no \r\n','2026-03-02 11:49:00',9,'[\"69a57995e8b0e_signature.PNG\"]',9,NULL),(15,'mer','cedi','CF4598','male','2026-03-27','mer@gmail.ma','0600000000','casa','','standard','2026-03-19',1,'none',260,'',NULL,'[]','review',NULL,'2026-03-02 14:10:13',9,'[]',9,'taxi'),(16,'alex','kira','ab1123','male','2026-03-06','alex44@gmail.ma','0600000000','casa','','standard','2026-03-06',1,'diabetes',200,'','','[]','approved',NULL,'2026-03-04 10:40:33',9,'[\"69a80c5609b61_The-Standard-CBIC-Medicaid-Card.png\"]',9,'health'),(17,'Ahmed','Test',NULL,NULL,NULL,'ahmed@test.com','0612345678','Rabat',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'[]','pending',NULL,'2026-03-06 15:33:48',NULL,'[]',NULL,'health');
+INSERT INTO `dossier` VALUES (11,'way2','way2','ae456598','male','2026-02-07','alex@gmail.ma','0600000000','way2','','basic','2026-01-31',1,'diabetes',6,'','','[\"cin\", \"contract\"]','approved',NULL,'2026-02-26 13:45:32',11,'[\"1772113532_726f7cb8_NumPy_Python_DS_2026.pdf\", \"1772113532_cab46071_DEVIS_AKSAM_CASQUE_IPPHONE_.pdf\"]',11,NULL,NULL,NULL,NULL),(12,'karim','li','ae456598','female','6999-05-15','karim@gmai.ma','0600000000','casa','','premium','1899-08-19',3,'hypertension',320,'','','[\"photo\", \"contract\"]','approved',NULL,'2026-02-26 15:51:22',9,'[\"1772121082_215cf502_PW1_-_Class_diagrams.pdf\", \"69a06c2c7cd13_signature.PNG\", \"69a06c2c7ce27_The-Standard-CBIC-Medicaid-Card.png\", \"69a06c3cecbe5_FICHIER DE PASSATIO.pdf\", \"69a06cdf74975_key programme.PNG\"]',9,NULL,NULL,NULL,NULL),(13,'Nada','kira','ab1123','female','1994-03-02','Nada@gmail.ma','0600000055','fes','','family','2026-04-01',1,'other',1500,'Cancer','','[]','approved',NULL,'2026-03-02 11:01:58',11,'[\"69a56ec24f618_The-Standard-CBIC-Medicaid-Card.png\"]',11,NULL,NULL,NULL,NULL),(14,'Mane','kalulu','D6925','male','1980-03-19','mane@gmail.ma','0600000055','casa','','basic','2026-03-19',1,'none',450,'','','[]','rejected','no \r\n','2026-03-02 11:49:00',9,'[\"69a57995e8b0e_signature.PNG\"]',9,NULL,NULL,NULL,NULL),(15,'mer','cedi','CF4598','male','2026-03-27','mer@gmail.ma','0600000000','casa','','standard','2026-03-19',1,'none',260,'',NULL,'[]','review',NULL,'2026-03-02 14:10:13',9,'[]',9,'taxi',NULL,NULL,NULL),(16,'alex','kira','ab1123','male','2026-03-06','alex44@gmail.ma','0600000000','casa','','standard','2026-03-06',1,'diabetes',200,'','','[]','approved',NULL,'2026-03-04 10:40:33',9,'[\"69a80c5609b61_The-Standard-CBIC-Medicaid-Card.png\"]',9,'health',NULL,NULL,NULL),(17,'Ahmed','Test',NULL,NULL,NULL,'ahmed@test.com','+212658398172','Rabat',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'[]','pending',NULL,'2026-03-06 15:33:48',NULL,'[]',NULL,'health',NULL,NULL,NULL),(20,'ahmed','laghzel','ae292777','male','2026-03-01','aminefoot100@gmail.com','+212658398172','sale','','standard','2026-04-01',NULL,NULL,91,NULL,NULL,'[]','approved',NULL,'2026-03-24 13:56:59',9,'[]',9,'taxi','2026-09-30','card',6),(21,'ahmed','laghzel','ae292777','male','2026-03-01','alex@gmail.ma','+212658398172','','','standard','2026-04-01',NULL,NULL,91,NULL,NULL,'[]','approved',NULL,'2026-03-24 14:43:47',9,'[]',9,'taxi','2026-09-30','card',6),(22,'ahmed','laghzel','ae292777','male','2004-01-06','way2@example.ma','+212658398172','casa','','basic','2026-04-01',NULL,NULL,100,NULL,NULL,'[]','approved',NULL,'2026-03-24 15:04:20',9,'[]',9,'taxi','2026-06-30','card',3),(23,'d','d','ae456598','','2026-03-01','alex@gmail.ma','+212658398172','','','basic','2026-04-01',NULL,NULL,100,NULL,NULL,'[]','approved',NULL,'2026-03-24 15:05:39',9,'[\"69c2a8615e107_contract_taxi_000022_laghzel.pdf\"]',9,'taxi','2026-06-30','card',3);
 /*!40000 ALTER TABLE `dossier` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -129,12 +132,12 @@ DROP TABLE IF EXISTS `prospect`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `prospect` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lastname` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone` int NOT NULL,
-  `email` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `cin` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `status` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cin` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -215,10 +218,11 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(180) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `roles` json NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone_number` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -230,7 +234,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Admin_OSS@gmail.ma','[\"ROLE_ADMIN\"]','$2y$13$ammus8p5bfJmGUfkbKACu.donoreoc5I/cCWVnU.r6N2G456jKrrC','ADMIN'),(4,'user3_OSS@gmail.ma','[\"ROLE_COMMERCIAL\"]','$2y$13$AXZjzTxH3a96MNCkxrF6luq4CmgpYuYyhhWkkVMux/QaTU1LnpKNC','user3'),(9,'Hana@gmail.ma','[\"ROLE_COMMERCIAL\"]','$2y$13$zUZ8TCcsTOKIzZfzcvIfnO9to35DwcBe11Vn2OafxrNK7hQU4NBY2','Hana'),(11,'way2@example.ma','[\"ROLE_COMMERCIAL\"]','$2y$13$Gy2Lrq7EnFHKz/TvBMQDMOnai/.Ct/gchkGbFrTqyXmCJEz29kNVS','way'),(17,'ahmed@gg.gg','[\"ROLE_ADMIN\"]','Ahmed123',NULL),(18,'cloud@gmail.com','[\"ROLE_COMMERCIAL\"]','$2y$13$Ma7V5OtLZWLmGNsNn2AkjOScmvYBXPzWjKQIEgjuL6PeYFh199HPi','Cloud');
+INSERT INTO `user` VALUES (1,'Admin_OSS@gmail.ma','[\"ROLE_ADMIN\"]','$2y$13$ammus8p5bfJmGUfkbKACu.donoreoc5I/cCWVnU.r6N2G456jKrrC','ADMIN',NULL),(4,'user3_OSS@gmail.ma','[\"ROLE_COMMERCIAL\"]','$2y$13$AXZjzTxH3a96MNCkxrF6luq4CmgpYuYyhhWkkVMux/QaTU1LnpKNC','user3',NULL),(9,'Hana@gmail.ma','[\"ROLE_COMMERCIAL\"]','$2y$13$zUZ8TCcsTOKIzZfzcvIfnO9to35DwcBe11Vn2OafxrNK7hQU4NBY2','Hana',NULL),(11,'way2@example.ma','[\"ROLE_COMMERCIAL\"]','$2y$13$Gy2Lrq7EnFHKz/TvBMQDMOnai/.Ct/gchkGbFrTqyXmCJEz29kNVS','way',NULL),(17,'ahmed@gg.gg','[\"ROLE_ADMIN\"]','Ahmed123',NULL,NULL),(18,'cloud@gmail.com','[\"ROLE_COMMERCIAL\"]','$2y$13$Ma7V5OtLZWLmGNsNn2AkjOScmvYBXPzWjKQIEgjuL6PeYFh199HPi','Cloud',NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -243,4 +247,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-03-09 10:46:38
+-- Dump completed on 2026-03-24 16:40:20
